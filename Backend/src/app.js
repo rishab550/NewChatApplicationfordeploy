4cca7b6858d5b32b59f8490a6fcd6d94987e15aa
Rishabh Garg <you@example.com>
2024-04-11 16:37:01 +0530

    test20

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -33,7 +33,7 @@ app.use("/api/v1/messages", messageRouter);
 app.use("/api/v1/active-users", activeUserRouter);
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "frontend", "vite-project", "dist", "index.html"),
+    path.join(__dirname, "Frontend", "vite-project", "dist", "index.html"),
   );
 });
 
